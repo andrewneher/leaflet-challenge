462182242f6b9509b78fe137e628a1c9937bc08d
Andrew Neher <andrewneher@gmail.com>
2021-04-19 15:19:18 -0500

    tutor session

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,28 +1,29 @@
 // Creating the map object
 var myMap = L.map("mapid", {
-    center: [40.7, -73.95],
-    zoom: 11
-  });
-  
-  // Adding the tile layer
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
-  
+  center: [40.7128, -74.0059],
+  zoom: 11
+});
 
-  
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+
+
 // Store the API query variables.
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
   
-var date = features.properties.time
-var feature
-var magnitude = features.properties.mag
-var size
-var place = features.properties.place
-var lat = features.geometry.coordinates[0]
-var long = features.geometry.coordinates[1]
-var depth = features.geometry.coordinates[2]
-
+// var date = features.properties.time
+// var feature
+// var magnitude = features.properties.mag
+// var size = 
+// var place = features.properties.place
+// var lat = features.geometry.coordinates[0]
+// var long = features.geometry.coordinates[1]
+// var depth = features.geometry.coordinates[2]
+// var earthquakes = []
 
 /////////////////////////////////////  
 //// Import & Visualize the Data ////
@@ -31,7 +32,50 @@ var depth = features.geometry.coordinates[2]
 // To do:
 
 // Create a map using Leaflet that plots all of the earthquakes from your data set based on their longitude and latitude.
+d3.json(baseURL).then(function(data) {
+  function styleInfo(feature){
+    return {  radius: getRadius(feature.properties.mag),
+      fillColor: getColor(feature.geometry.coordinates[0]),
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8};
+  
+  }
+  function getColor(depth){
+    switch(true){
+      case depth>90:
+        return "red";
+      case depth>70:
+        return "orange";
+      case depth>50:
+        return "yellow";
+      case depth>30:
+        return "ltgreen";
+      case depth>10:
+        return "green";
+      default: 
+        return "white";
+    }
+  }
+  function getRadius(magnitude){
+      if (magnitude==0) {
+        return 1;
+      }
+      return magnitude*4;
+  
+  }
+  L.geoJSON(data, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng);
+    },
+  style: styleInfo,
+  onEachFeture: function (feature,layer){
+    layer.bindPopUp(feature.properties.mag + feature.geometry.coordinates[2])
+  }
+  }).addTo(myMap);
 
+});
 
 // Your data markers should reflect the magnitude of the earthquake by their size and and depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger and earthquakes with greater depth should appear darker in color.
 
